f4cb0608c13c577814b25c9153f9e75cd90f1bc4
suurang <44763020+suurang@users.noreply.github.com>
2018-12-03 22:33:46 +0900

    Update main.js
    add jquery

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
-var navItem = document.querySelectorAll('.tab-nav li');
-var navDeco = document.querySelector('.tab-nav li.bgbox');
+/*
+var navItems = document.querySelectorAll('.tab-nav li');
+var navDeco = document.querySelector('.tab-nav span');
 var tabPanel = document.querySelectorAll('.tab-panel');
 
-for (var i = 0; i < navItem.length; i++) {
-	navItem[i].addEventListener('click', function () {
-		/* (이벤트 이름, 실행 해당 함수) */
-        var clickIndex = this.dataset.index;
-        navDeco.style.left = clickIndex * 120 + 'px';
-        document.querySelector('.active').classList.remove('active');
-        this.classList.add('active');
-        document.querySelector('.current').classList.remove('current');
-        tabPanel[clickIndex].classList.add('current');   
-	});   
+for(var i = 0; i < navItems.length; i++) {
+	navItems[i].addEventListener('click', function (e) {
+		navDeco.style.left = (120 * e.currentTarget.dataset.index) + 'px';
+		document.querySelector('.active').classList.remove('active');
+		document.querySelector('.current').classList.remove('current');
+		tabPanel[e.currentTarget.dataset.index].classList.add('current');
+		e.currentTarget.classList.add('active');
+	});	
 }
+*/
+var navItems = $('.tab-nav li'),
+    navDeco = $('.tab-nav span'),
+    tabPanel = $('.tab-panel');
+
+navItems.click(function(){
+    var idx = $(this).data('index');
+    navDeco.css('left', 120 * idx + 'px');
+    $('.active').removeClass('active');
+    $(this).addClass('active');
+    $('.current').removeClass('current');
+    tabPanel.eq(idx).addClass('current');
+});
